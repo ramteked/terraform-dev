136213d4c0f8ca73a59f53a77761291c3c8a77cf
Devendra Rameke <41633774+ramteked@users.noreply.github.com>
2025-09-21 22:31:10 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
  region = "us-east-2"
 }
 
-resource "aws_instace" "one" {
+resource "aws_instance" "one" {
    ami = "ami-0ca4d5db4872d0c28"
    instance_type = "t3.micro"
    tags = {
